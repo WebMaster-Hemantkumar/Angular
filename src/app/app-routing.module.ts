fe6906025eeef65e376d336fe10c79bc769d1866
santoshir <hemantbly.kumar@gmail.com>
2019-08-11 12:14:49 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { AboutComponent } from './about/about.component';
+import { PricingComponent } from './pricing/pricing.component';
+import { TestimonialComponent } from './testimonial/testimonial.component';
+import { NewsComponent } from './news/news.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'Home',component:HeaderComponent},
+{path:'Home',component:HeaderComponent},
+{path:'AboutUS',component:AboutComponent},
+{path:'Pricing',component:PricingComponent},
+{path:'Testimonial',component:TestimonialComponent},
+{path:'News',component:NewsComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { AboutComponent } from './about/about.component';
+import { PricingComponent } from './pricing/pricing.component';
+import { TestimonialComponent } from './testimonial/testimonial.component';
+import { NewsComponent } from './news/news.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AboutComponent,
+    PricingComponent,
+    TestimonialComponent,
+    NewsComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
